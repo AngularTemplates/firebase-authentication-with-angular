f101ba66e7cdb5f0a67153dc5628f83d9c08eca8
vinothMcc <vinoth@entrayn.com>
2018-12-14 17:24:29 +0530

    add route for collectionlist

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AuthService } from '../core/auth.service'
+import { AuthService } from '../core/auth.service';
 import { Router, Params } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
@@ -9,7 +9,6 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['login.scss']
 })
 export class LoginComponent {
-
   loginForm: FormGroup;
   errorMessage: string = '';
 
@@ -23,39 +22,38 @@ export class LoginComponent {
 
   createForm() {
     this.loginForm = this.fb.group({
-      email: ['', Validators.required ],
-      password: ['',Validators.required]
+      email: ['', Validators.required],
+      password: ['', Validators.required]
     });
   }
 
-  tryFacebookLogin(){
-    this.authService.doFacebookLogin()
-    .then(res => {
+  tryFacebookLogin() {
+    this.authService.doFacebookLogin().then(res => {
       this.router.navigate(['/user']);
-    })
+    });
   }
 
-  tryTwitterLogin(){
-    this.authService.doTwitterLogin()
-    .then(res => {
+  tryTwitterLogin() {
+    this.authService.doTwitterLogin().then(res => {
       this.router.navigate(['/user']);
-    })
+    });
   }
 
-  tryGoogleLogin(){
-    this.authService.doGoogleLogin()
-    .then(res => {
+  tryGoogleLogin() {
+    this.authService.doGoogleLogin().then(res => {
       this.router.navigate(['/user']);
-    })
+    });
   }
 
-  tryLogin(value){
-    this.authService.doLogin(value)
-    .then(res => {
-      this.router.navigate(['/user']);
-    }, err => {
-      console.log(err);
-      this.errorMessage = err.message;
-    })
+  tryLogin(value) {
+    this.authService.doLogin(value).then(
+      res => {
+        this.router.navigate(['/user']);
+      },
+      err => {
+        console.log(err);
+        this.errorMessage = err.message;
+      }
+    );
   }
 }
