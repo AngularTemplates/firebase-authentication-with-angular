2d6345cc3bbd35df70832b9093c776a3c6938434
Dayana Jabif <dayana.jabif@gmail.com>
2019-05-08 15:11:38 -0300

    Add missing semicolon

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -75,7 +75,7 @@ export class AuthService {
   doLogout(){
     return new Promise((resolve, reject) => {
       if(firebase.auth().currentUser){
-        this.afAuth.auth.signOut()
+        this.afAuth.auth.signOut();
         resolve();
       }
       else{
